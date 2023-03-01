9d73ea75d5da80bbb2bcfc4e3d5f4019ae19c4b3
Fredricc <kfred45.fk@gmail.com>
2023-03-01 06:58:41 +0300

    Demo.Returning Resources Part 1

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,15 +1,18 @@
-﻿
-
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
 {
     [ApiController]
     public class CitiesController : ControllerBase
     {
-        public IActionResult GetCities()
+        public JsonResult GetCities()
         {
-
+            return new JsonResult(
+                new  List<object>
+                {
+                    new { id = 1 , Name = "New York City"},
+                    new { id = 2 , Name = "Antwerp" }
+                });
         }
     }
 }
