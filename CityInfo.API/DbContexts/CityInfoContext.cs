b345a346eb4c0b2a9b42e878a69b028b178fcf96
Fredricc <kfred45.fk@gmail.com>
2023-03-07 06:30:18 +0300

    Demo.Seeding the Database with Data

--- CityInfo.API/DbContexts/CityInfoContext.cs
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -14,5 +14,90 @@ namespace CityInfo.API.DbContexts
         {
         
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>()
+                .HasData(
+                new City("New York City")
+                {
+                    Id = 1,
+                    Description = "The one with that big Park."
+                },
+                new City("Antwerp")
+                {
+                    Id =2 ,
+                    Description = "The one the cathedral that was never really finished."
+                },
+                 new City("Paris")
+                 {
+                     Id = 3,
+                     Description = "The one with the big tower."
+                 },
+                  new City("Nairobi")
+                  {
+                      Id = 4,
+                      Description = "The one with that big National Park."
+                  }
+
+                );
+            modelBuilder.Entity<PointOfInterest>()
+                 .HasData(
+                new PointOfInterest("Central Park")
+                {
+                    Id = 1,
+                    CityId = 1,
+                    Description = "The most visited urban park in the United States."
+                },
+               new PointOfInterest("Empire State Building")
+               {
+                   Id = 2,
+                   CityId = 1,
+                   Description = "A 102-story skyscraper located in Midtown Manhattan."
+               },
+                 new PointOfInterest("Cathedral")
+                 {
+                     Id = 3,
+                     CityId = 2,
+                     Description = "A Gothic style cathedral, conceived by architects Jan and Pieter Appelmans."
+                 },
+               new PointOfInterest("Antwerp Central Station")
+               {
+                   Id = 4,
+                   CityId = 2,
+                   Description = "The finest example of railway architecture in Belgium."
+               },
+               new PointOfInterest("Eiffel Tower")
+               {
+                   Id = 5,
+                   CityId = 3,
+                   Description = "A wrought iron lattice tower on the Champ de Mars, named after engineer Gustave Eiffel."
+               },
+               new PointOfInterest("The Louvre")
+               {
+                   Id = 6,
+                   CityId = 3,
+                   Description = "The world's largest museum."
+               }
+               new PointOfInterest("KICC")
+               {
+                   Id = 7,
+                   CityId = 3,
+                   Description = "A wrought iron lattice tower on the Champ de Mars, named after engineer Gustave Eiffel."
+               },
+               new PointOfInterest("The National Museum")
+               {
+                   Id = 8,
+                   CityId = 3,
+                   Description = "The world's largest museum."
+               }
+               );
+            base.OnModelCreating(modelBuilder);
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite("connectionstring");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
     }
 }
