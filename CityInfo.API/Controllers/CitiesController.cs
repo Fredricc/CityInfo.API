8aefee648d2c7cbaa866c51db0781ae4794180df
Fredricc <kfred45.fk@gmail.com>
2023-03-15 22:03:01 +0300

    Demo.Filtering Resources

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -24,7 +24,8 @@ namespace CityInfo.API.Controllers
         }
 
         [HttpGet]
-        public async Task< ActionResult<IEnumerable<CityDto>>> GetCities()
+        public async Task< ActionResult<IEnumerable<CityDto>>> GetCities(
+            string? name)
         {
             var cityEntities = await _cityInfoRepository.GetCitiesAsync();
 
